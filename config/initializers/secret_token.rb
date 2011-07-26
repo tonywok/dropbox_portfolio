c837bde789945f13b651ff9e92115ecfc501851d
Tony Schneider <tonywok@gmail.com>
2011-07-26 09:17:59 -0400

    initial rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fill::Application.config.secret_token = 'b309683e3d6df15c076821e45a2e5ee82ffb9147fd1c78569247f77164826540dc326ed09697c929eded3ee128131593272acae637ab151705f34b2c733d179c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Fill::Application.config.session_store :cookie_store, key: '_fill_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Fill::Application.config.session_store :active_record_store
