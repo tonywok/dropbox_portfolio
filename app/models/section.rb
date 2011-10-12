cf4d93c7281a292f2a0517293807adc0580b4df4
Tony Schneider <tonywok@gmail.com>
2011-10-11 22:46:51 -0400

    Destroying a section destroys its dropbox_files

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,7 +2,7 @@ class Section < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, :use => :slugged
 
-  has_many :dropbox_files
+  has_many :dropbox_files, :dependent => :destroy
 
   validates :dropbox_files, :presence => true
   validates :name, :uniqueness => true,
