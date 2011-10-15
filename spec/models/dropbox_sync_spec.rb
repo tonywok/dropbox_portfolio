a81c73cf5c1734cd7f0bb4cc342d786313154b3d
Tony Schneider <tonywok@gmail.com>
2011-10-15 13:42:45 -0400

    fix tests for DropboxSync#download

--- spec/models/dropbox_sync_spec.rb
+++ spec/models/dropbox_sync_spec.rb
@@ -57,52 +57,33 @@ describe "DropboxSync" do
     end
   end
 
-  # describe "#download_new" do
-  #   let(:section_name) { 'print' }
-  #   let(:revision) { '1' }
-  #   let(:meta_path) { "/foo/bar/columbus-brewery-redesign.png" }
-  #   let(:new_meta_path) { "!#{meta_path}" }
+  describe "#download" do
+    let(:section_name) { 'print' }
+    let!(:section) { Factory.build(:section, :name => section_name) }
+    let(:remote_data) do
+      { "name"          => "#{section_name}",
+        "description"   => "lorem ipsum dolar",
+        "dropbox_files" => "[{\"revision\":1041066054,\"thumb_exists\":true,\"bytes\":76278,\"modified\":\"2011-07-30T19:47:32-04:00\",\"path\":\"/test/mewithmustache.jpeg\",\"is_dir\":false,\"icon\":\"page_white_picture\",\"mime_type\":\"image/jpeg\",\"size\":\"74.5KB\",\"directory?\":false},
+                             {\"revision\":1041065999,\"thumb_exists\":true,\"bytes\":567324,\"modified\":\"2011-08-20T12:15:16-04:00\",\"path\":\"/test/nancers.jpeg\",\"is_dir\":false,\"icon\":\"page_white_picture\",\"mime_type\":\"image/jpeg\",\"size\":\"554KB\",\"directory?\":false}]" }
+    end
+    let(:session) { mock('session', :download => "Remote Content") }
+    let(:dropbox) { DropboxSync.new(session, remote_data) }
 
-  #   let(:meta) do
-  #     [{"revision" => revision, "thumb_exists" => true, "bytes" => 5161,  "modified" => '2011-07-31 18:04:59 -0400', "path" => new_meta_path, "is_dir" => false, "icon" => "page_white_picture", "mime_type" => "image/png", "size" => "5KB", "directory" => false}]
-  #   end
+    it "adds the remote dropbox files to section " do
+      new_dropbox_filepaths = dropbox.remote_dropbox_files.map { |rdf| rdf["path"] }
+      dropbox.download(dropbox.remote_dropbox_files)
 
-  #   let(:session) { mock('session', :ls => meta, :download => "Content") }
+      section.dropbox_files.each do |dropbox_file|
+        new_dropbox_filepaths.should include dropbox_file.meta_path
+      end
+    end
 
-  #   let(:dropbox) do
-  #     dropbox = DropboxSync.new(session, section_name)
-  #     dropbox.meta = meta
-  #     dropbox
-  #   end
+    it "creates a file on the file system" do
+      dropbox.download(dropbox.remote_dropbox_files)
 
-  #   context "the file belongs to an existing section" do
-  #     let!(:section) { Factory(:section, :name => section_name) }
-
-  #     it "attaches the file to the section" do
-  #       dropbox.download_new
-  #       dropbox_filepaths = section.dropbox_files.map(&:meta_path)
-  #       dropbox_filepaths.should include new_meta_path
-  #     end
-
-  #     it "creates a file on the file system" do
-  #       dropbox.download_new
-  #       File.exists?("#{section.dropbox_files.first.attachment}")
-  #     end
-  #   end
-
-  #   context "the file belongs to a new section" do
-  #     it "attaches the file to the new item" do
-  #       dropbox.download_new
-  #       new_section = Section.find_by_name(section_name)
-  #       dropbox_filepaths = new_section.dropbox_files.map(&:meta_path)
-  #       dropbox_filepaths.should include new_meta_path
-  #     end
-
-  #     it "creates a file on the file system" do
-  #       dropbox.download_new
-  #       new_section = Section.find_by_name(section_name)
-  #       File.exists?("#{new_section.dropbox_files.first.attachment}")
-  #     end
-  #   end
-  # end
+      section.dropbox_files.each do |dropbox_file|
+        File.exists?("#{dropbox_file.attachment}").should be_true
+      end
+    end
+  end
 end
