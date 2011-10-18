e4cc149a689f41482b0fa872f2340db84ffcf85e
Tony Schneider <tonywok@gmail.com>
2011-10-18 00:40:31 -0400

    debugging

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,8 @@ require 'bundler/capistrano'
 
 set :application, "Dropbox Portfolio"
 set :repository, "git@github.com:tonywok/dropbox_portfolio.git"
+set :keep_releases, 7
+
 set :deploy_to, "/var/www/dropbox_portfolio"
 set :domain, "echo.agrieser.net"
 
@@ -26,8 +28,6 @@ role :db, domain, :primary => true
 set :rails_env, "production"
 set :branch, "production"
 
-set :shared_assets, "public/portfolio"
-
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
@@ -37,21 +37,24 @@ namespace :deploy do
 end
 
 namespace :uploads do
-  desc "Creates the upload folders unless they exist, sets permissions"
+  desc "Creates the portfolio folders unless they exist, sets permissions"
   task :setup, :except => { :no_release => true } do
     dirs = uploads_dirs.map { |d| File.join(shared_path, d) }
+    puts "*" * 100
+    puts dirs
+    puts "*" * 100
     run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
   end
 
-  desc "Creates the symlink to uploads shared folder for most recent version"
+  desc "Creates the symlink to portfolio shared folder for most recent version"
   task :symlink, :except => { :no_release => true } do
     run "rm -rf #{release_path}/public/portfolio"
-    run "ln -nfs #{shared_path}/uploads #{release_path}/public/portfolio"
+    run "ln -nfs #{shared_path}/portfolio #{release_path}/public/portfolio"
   end
 
   desc "Computes uploads directory paths and registers them in Capistrano environment"
   task :register_dirs do
-    set :uploads_dirs,    %w(uploads)
+    set :uploads_dirs,    %w(portfolio)
     set :shared_children, fetch(:shared_children) + fetch(:uploads_dirs)
   end
 
